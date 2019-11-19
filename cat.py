a7578501a448682a8c61ebcb69612fba1b3b9d9a
Chris Aquino <chrisaquino@radishmouse.local>
2019-11-19 09:53:49 -0500

    adds cuddle-abilities

--- cat.py
+++ cat.py
@@ -21,19 +21,30 @@
 # bundling related data (like a dictionary)
 # with the functions that modify that data
 
+from random import randint
+
 class Cat:
     # In __init__ python allows you to
     # refer to the object as it's being created!
     # Cool! You can customize that cat. Prrrrrr....
-    def __init__(self, new_name):
+    def __init__(self, new_name, friendliness=0.5, happiness=10, cuddle_power=1):
         self.name = new_name
+        self.friendliness = friendliness
+        self.happiness = happiness
+        self.cuddle_power = cuddle_power
 
     def greet(self, whom):
         print(f"Hello, I am {self.name}. Nice to meet you, {whom.name}!")
 
     def cuddle(self, whom):
-        pass
+        cuddle_chance = randint(0, 10)/10
+        if cuddle_chance <= self.friendliness:
+            print(f"I cuddle you, {whom.name}!")
+            whom.happiness += self.cuddle_power
+        else:
+            print(f"Bad touch, bad touch!")
 
-othercat = Cat("Milla")
-mycat = Cat("Oakley") # Use the class to create a new Cat "instance"
-                # "Instance" is another word for "object"
+milla = Cat("Milla")
+oakley = Cat("Oakley") 
+# Use the class to create a new Cat "instance"
+# "Instance" is another word for "object"
